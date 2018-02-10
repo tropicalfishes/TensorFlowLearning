a2e8a557aa84733869a255b56008e301488ca955
tropicalfish <rdygzs@163.com>
2018-02-10 21:10:41 +0800

    增加mnist遍历功能

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 import tensorflow as tf
-from mnist.train import images, labels
+import mnist.train
+import mnist.test
 
 def Info(s):
     print("[Script：]%s" % s)
@@ -8,7 +9,27 @@ def Info(s):
 
 def Main():
     Info("Start TensorFlow")
+    x = tf.placeholder(tf.float32, [None, 784])
+    W = tf.Variable(tf.zeros([784, 10]))
+    b = tf.Variable(tf.zeros([10]))
+    y = tf.nn.softmax(tf.matmul(x, W) + b)
 
+    y_ = tf.placeholder("float", [None, 10])
+    cross_entropy = -tf.reduce_sum(y_ * tf.log(y))
+
+    train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
+
+    init = tf.initialize_all_variables()
+    sess = tf.Session()
+    sess.run(init)
+
+    for i in range(1000):
+        batch_xs, batch_ys = mnist.train.next_batch(100)
+        sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+
+    correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+    print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
 
 if __name__ == "__main__":
     Main()
--- mnist/train/__init__.py
+++ mnist/train/__init__.py
@@ -7,4 +7,15 @@ labels_file_path = os.path.join(os.getcwd(), "mnist", "train", "train-labels-idx
 images = extract_images(images_file_path)
 labels = extract_labels(labels_file_path)
 
+g_LastIndex = 0
+
+def next_batch(batch_size, fake_data=False):
+    if fake_data:
+        fake_image = [1] * 784
+        fake_label = 0
+        return [fake_image for _ in range(batch_size)], [
+            fake_label for _ in range(batch_size)
+        ]
+
+
 
